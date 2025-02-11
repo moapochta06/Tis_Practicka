45174a33a6bf037c12edfe481a547835fe0b4515
moapochta06 <90915948+moapochta06@users.noreply.github.com>
2025-02-11 17:08:10 +0700

    commit

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -9,14 +9,12 @@ class CollectTest extends TestCase
     {
         $data = new Collect(['a' => 1, 'b' => 2, 'c' => 3]);
         $keys = $data->keys();
-        $this->assertInstanceOf(Collect::class, $keys);
         $this->assertSame(['a', 'b', 'c'], $keys->toArray());
     }
     public function testValues(): void
     {
         $data = new Collect(['a' => 1, 'b' => 2, 'c' => 3]);
         $values = $data->values();
-        $this->assertInstanceOf(Collect::class, $values);
         $this->assertSame([1, 2, 3], $values->toArray());
     }
     public function testGet(): void
@@ -30,7 +28,6 @@ class CollectTest extends TestCase
     {
         $data = new Collect(['a' => 1, 'b' => 2, 'c' => 3]);
         $excepted = $data->except('a');
-        $this->assertInstanceOf(Collect::class, $excepted);
         $this->assertSame(['b' => 2, 'c' => 3], $excepted->toArray());
     }
 }
